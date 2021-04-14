4cd1b689e70d11d9dc5e8fbb1d5ee9b5379c5273
johnfasou <johnfasou@gmail.com>
2021-04-14 18:15:58 +0300

    create one product

---
+++ README.md
@@ -0,0 +1,7 @@
+# woo-import.go
+
+Project to do various jobs for woocommerce using wp-cli
+- Remove all existing products
+- Import products from "products.csv" file
+- Remove unattached images
+
--- src/runner.go
+++ src/runner.go
@@ -7,17 +7,29 @@ import (
 	"os/exec"
 	"regexp"
 	"strings"
-	//  "log"
 )
 
+var productsCsv_file = "./products.csv "
+
 func main() {
-	// app()
-	executeShellCommand()
+	// dir, err := os.Getwd()
+	// if err != nil {
+	//	log.Fatal(err)
+	// 	fmt.Println(err)
+	//  fmt.Println(err.Error())
+	// }
+	// fmt.Println(dir)
+
+	app()
+	// executeShellCommand()
 }
 
 func app() {
+	// Remove all products
+	// removeAllProducts()
+
 	// csvReader("./products.csv")
-	res := csvReader("c:/_/.Sync/_.Work/wp-cli/woo-import/golang/src/products.csv ")
+	res := csvReader(productsCsv_file)
 
 	fmt.Println("Total rown: ", len(res))
 	fmt.Println("Total columns: ", len(res[0]))
@@ -31,32 +43,10 @@ func app() {
 		// fmt.Println(i, "|", head[i])
 	}
 
-	template := createProduct_processor(res, head, createProduct_template)
-	fmt.Println(template)
-}
+	createProduct_template := createProduct_processor(res, head, createProduct_template)
+	fmt.Println(createProduct_template)
+	executeShellCommand(createProduct_template)
 
-func executeShellCommand() {
-	app := "ll"
-	cmd := exec.Command(app)
-
-	// app := "echo"
-	// arg0 := "-e"
-	// arg1 := "Hello world"
-	// arg2 := "\n\tfrom"
-	// arg3 := "golang"
-	// cmd := exec.Command(app, arg0, arg1, arg2, arg3)
-
-	stdout, err := cmd.Output()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Print(string(stdout))
-}
-
-func removeProducts() {
 }
 
 func removeUnattachedImages() {
@@ -84,65 +74,88 @@ func createProduct_processor(res [][]string, head map[int]string, template strin
 	return out
 }
 
-var deleteAllProducts_template string = `
-wp wc product create  --user=johnny \
-
-`
-
 // --status, should be one of: draft, pending, private, publish, future
 var createProduct_template string = `
-wp wc product create  --user=johnny \
-	--name="{{Name}}" \
-	--type="{{Type}}" \
-	--sku="{{SKU}}" \
-	--slug="{{}}" \
-	
-	--status="publish" \
+	wp wc product create  --user=1 --path="/home/beanstalkweb/public_html/porto" \
+		--name="{{Name}}" \
+		--type="{{Type}}" \
+		--sku="{{SKU}}" \
+		--slug="{{}}" \
+		
+		--status="publish" \
 
-	--featured="{{Is featured?}}" \
-	--catalog_visibility="{{Visibility in catalog}}"\
-	--description="{{Description}}" \
-	--short_description="{{Short description}}" \
-	--regular_price="{{Regular price}}" \
-	--sale_price="{{Sale price}}" \
-	--date_on_sale_from="{{Date sale price starts}}" \
-	--date_on_sale_from_gmt="{{}}" \
-	--date_on_sale_to="{{Date sale price ends}}" \
-	--date_on_sale_to_gmt="{{}}" \
-	--virtual="{{}}" \
-	--downloadable="{{}}" \
-	--downloads="{{}}" \
-	--download_limit="{{}}" \
-	--download_expiry="{{}}" \
-	--external_url="{{}}" \
-	--button_text="{{}}" \
-	--tax_status="{{Tax status}}" \
-	--tax_class="{{Tax class}}" \
-	--manage_stock="{{}}" \
-	--stock_quantity="{{}}" \
-	--in_stock="{{In stock?}}" \
-	--backorders="{{Backorders allowed?}}" \
-	--sold_individually="{{Sold individually?}}" \
-	--weight="{{Weight (kg)}}" \
-	--dimensions="{{}}" \
-	--shipping_class="{{Shipping class}}" \
-	--reviews_allowed="{{Allow customer reviews?}}" \
-	--upsell_ids="{{Upsells}}" \
-	--cross_sell_ids="{{Cross-sells}}" \
-	--parent_id="{{Parent}}" \
-	--purchase_note="{{}}" \
-	--categories="{{Categories}}" \
-	--tags="{{Tags}}" \
-	--images="{{Images}}" \
-	--attributes="{{}}" \
-	--default_attributes="{{}}" \
-	--grouped_products="{{}}" \
-	--menu_order="{{}}" \
-	--meta_data="{{}}" \
-	--wpml_language="{{}}" \
-	--porcelain
+		--featured="{{Is featured?}}" \
+		--catalog_visibility="{{Visibility in catalog}}"\
+		--description="{{Description}}" \
+		--short_description="{{Short description}}" \
+		--regular_price="{{Regular price}}" \
+		--sale_price="{{Sale price}}" \
+		--date_on_sale_from="{{Date sale price starts}}" \
+		--date_on_sale_from_gmt="{{}}" \
+		--date_on_sale_to="{{Date sale price ends}}" \
+		--date_on_sale_to_gmt="{{}}" \
+		--virtual="{{}}" \
+		--downloadable="{{}}" \
+		--downloads="{{}}" \
+		--download_limit="{{}}" \
+		--download_expiry="{{}}" \
+		--external_url="{{}}" \
+		--button_text="{{}}" \
+		--tax_status="{{Tax status}}" \
+		--tax_class="{{Tax class}}" \
+		--manage_stock="{{}}" \
+		--stock_quantity="{{}}" \
+		--in_stock="{{In stock?}}" \
+		--backorders="{{Backorders allowed?}}" \
+		--sold_individually="{{Sold individually?}}" \
+		--weight="{{Weight (kg)}}" \
+		--dimensions="{{}}" \
+		--shipping_class="{{Shipping class}}" \
+		--reviews_allowed="{{Allow customer reviews?}}" \
+		--upsell_ids="{{Upsells}}" \
+		--cross_sell_ids="{{Cross-sells}}" \
+		--parent_id="{{Parent}}" \
+		--purchase_note="{{}}" \
+		--categories="{{Categories}}" \
+		--tags="{{Tags}}" \
+		--images="{{Images}}" \
+		--attributes="{{}}" \
+		--default_attributes="{{}}" \
+		--grouped_products="{{}}" \
+		--menu_order="{{}}" \
+		--meta_data="{{}}" \
+		--wpml_language="{{}}" \
+		--porcelain
 
 `
+
+func removeAllProducts() {
+	executeShellCommand(removeAllProducts_template)
+}
+
+var removeAllProducts_template string = `
+	wp post delete $(wp wc product list --user=1 --format=ids) --force
+`
+
+func executeShellCommand(cmdLine string) {
+	// cmdLine := "ll"
+	cmd := exec.Command(cmdLine)
+
+	// app := "echo"
+	// arg0 := "-e"
+	// arg1 := "Hello world"
+	// arg2 := "\n\tfrom"
+	// arg3 := "golang"
+	// cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Print(string(stdout))
+}
 
 func csvReader(file string) [][]string {
 	// 1. Open the file
